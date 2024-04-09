8834243b833bdd0b45690f765eceee4456e734f5
Usuario <Usuario@CPD01-M01>
2024-04-09 11:29:48 -0300

    Adicionar arquivos de projeto.

---
+++ DoWhile/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DoWhile
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string req;
+            double valor, total;
+            do
+            {
+                Console.ForegroundColor = ConsoleColor.DarkBlue;
+                Console.WriteLine("O que será comprado:");
+                Console.ResetColor();
+                Console.ReadLine();
+                Console.ForegroundColor = ConsoleColor.DarkBlue;
+                Console.WriteLine("Quanto será gasto:");
+                Console.ResetColor();
+                valor = double.Parse(Console.ReadLine());
+                total = +valor;
+                Console.ForegroundColor = ConsoleColor.Yellow;
+                Console.WriteLine("DESEJA CONTINUAR? S/N");
+                Console.ResetColor();
+                req = Console.ReadLine().ToUpper();
+            }
+            while (req == "S");
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("Valor total:"+total.ToString("C"));
+            Console.ResetColor();  
+            Console.ReadKey();
+        }
+    }
+}
---
+++ TesteDeVariasCoisas/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TesteDeVariasCoisas
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+               while (true) 
+                 {
+                for (int i = 1; i < 15; i++)
+                { 
+                    Console.ForegroundColor = (ConsoleColor)i  ;
+                     Console.Write("");
+                     }
+                 }
+            }
+    }
+}
